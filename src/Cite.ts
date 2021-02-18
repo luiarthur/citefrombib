5cda0b22fd9ea0d1a6924c6d4fa38d53eee96c4a
Arthur Lui <luiarthur@gmail.com>
2021-02-17 22:46:01 -0800

    added some basic functionality.

--- src/Cite.ts
+++ src/Cite.ts
@@ -12,12 +12,21 @@ export class Cite {
   }
 
   parseAuthor(author: string) {
+    let lastName = ""
+    let givenName = ""
     if (author.includes(',')) {  // Last name, first name and ...
-      const lastname = author.trim().split(',')[0]
+      [lastName, givenName] = author.trim().split(',')
     } else { // First name last name and ...
-      const lastname = author.trim().split(' ').slice(-1)[0]
+      const fullname = author.trim().split(' ')
+      const lname = fullname.splice(-1)
+      lastName = lname[0]
+      givenName = fullname.join(' ')
     }
-    return {lastname: lastname}
+    return {lastName: lastName.trim(), givenName: givenName.trim()}
+  }
+
+  citep(id: string, f: () => string = () => "", add: boolean=true): string {
+    return this.cite(id, true, f, add)
   }
 
   // Make an infline references (like `\cite` and `\citep` in latex).
@@ -27,17 +36,37 @@ export class Cite {
     const authors = this.refs[id].author.split(' and ').map(this.parseAuthor)
     const year = this.refs[id].year
     if (authors.length == 1) {
-      citation += `${authors[0].lastname} [${year}]`
+      citation += `${authors[0].lastName} [${year}]`
     } else if (authors.length == 2) {
-      citation += `${authors[0].lastname} and ${authors[1].lastname} [${year}]`
+      if (paren) {
+        citation += `(${authors[0].lastName} and ${authors[1].lastName} ${year})`
+      } else {
+        citation += `${authors[0].lastName} and ${authors[1].lastName} (${year})`
+      }
     } else {
-      citation += `${authors[0].lastname} et. al [${year}]`
+      if (paren) {
+        citation += `(${authors[0].lastName} et al. ${year})`
+      } else {
+        citation += `${authors[0].lastName} et al. (${year})`
+      }
     }
-    this.citedItems.add(id)
+    add && this.citedItems.add(id)
     return citation
+  }
+
+  bibitem(id: string, format={}) {
+    // TODO: handle the format.
+    let {author, year, title, journal} = this.refs[id]
+    let authors = author.split(' and ').map(this.parseAuthor)
+                        .map(_ => [_.givenName, _.lastName].join(' '))
+    let lastAuthor = authors.splice(-1)
+    let allAuthors = authors.join(', ') + ', and ' + lastAuthor[0]
+    return `${allAuthors}. ${title}. ${journal}, ${year}`
   }
 
   // Generate the bibliography.
   bibliography(format={}) {
+    // TODO: handle the format.
+    return Array.from(this.citedItems).map(id => this.bibitem(id, format))
   }
 }
--- src/__test__/Cite.test.ts
+++ src/__test__/Cite.test.ts
@@ -3,10 +3,24 @@ import * as bib from './bib.json'
 
 test("", () => {
   const citer = new Cite(bib)
-  console.log(citer.refs.advi)
-  console.log(citer.cite("advi"))
-  console.log(citer.cite("lui2020bayesian"))
-  console.log(citer.citedItems)
 
-  expect(1).toEqual(1)
+  // Sanity check: print one bibentry.
+  // console.log(citer.refs.advi)
+
+  // Check citations are correctly formatted.
+  expect(citer.cite("advi")).toEqual("Kucukelbir et al. (2017)")
+  expect(citer.cite("lui2020bayesian")).toEqual("Lui et al. (2020)")
+
+  // Check parenthesized citations are correctly formatted.
+  expect(citer.citep("advi")).toEqual("(Kucukelbir et al. 2017)")
+  expect(citer.citep("lui2020bayesian")).toEqual("(Lui et al. 2020)")
+
+  // Check that the cited items are being tracked.
+  expect(citer.citedItems).toEqual(new Set(["advi", "lui2020bayesian"]))
+
+  // Check number of cited items is correct.
+  expect(citer.citedItems.length == 2)
+
+  // Print a sample bibliography.
+  console.log(citer.bibliography().join('\n'))
 })
